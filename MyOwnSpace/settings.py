6803a3c809867443d7be07206775fdb66ed3300c
almatsy159 <drago.gl159@gmail.com>
2024-06-10 18:14:48 +0200

    re setting up whitenoise

--- MyOwnSpace/settings.py
+++ MyOwnSpace/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # https://docs.render.com/deploy-django
-
+"""
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+"""
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

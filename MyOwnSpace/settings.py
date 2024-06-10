add0811e2b8912b0b698b59a3276fe8f820525df
almatsy159 <drago.gl159@gmail.com>
2024-06-10 17:25:15 +0200

    setting up psql db

--- MyOwnSpace/settings.py
+++ MyOwnSpace/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }}
 }
 """
-DATABASES = {'default': dj_database_url.config(default='postgres://mysite:GaVhOSk5ElvgOrLAesJYXXkRU0pkHJB2@dpg-cpg82e7109ks73ca5d90-a/mysite_90d6/MyOwnSpace',conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config(default='postgres://mysite:GaVhOSk5ElvgOrLAesJYXXkRU0pkHJB2@dpg-cpg82e7109ks73ca5d90-a/mysite_90d6/MyOwnSpace/',conn_max_age=600)}
 #"service":"api",
 #            "passfile":".pswd"
 

0a4489a297cb6bedd1bb6b6faf954d1c8f3425c1
almatsy159 <drago.gl159@gmail.com>
2024-06-11 09:37:20 +0200

    correcting static root issue

--- MyOwnSpace/settings.py
+++ MyOwnSpace/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = os.path.join(BASE_DIR,'static')
 # https://docs.render.com/deploy-django
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

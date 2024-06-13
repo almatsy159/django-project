fdeaaf13aaf63febc0f353dc2cadd4037628767c
almatsy159 <drago.gl159@gmail.com>
2024-06-13 09:31:39 +0200

    updating site

--- MyOwnSpace/settings.py
+++ MyOwnSpace/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR,'static')
+STATIC_URL = os.path.join(BASE_DIR,'static/')
 
 # https://docs.render.com/deploy-django
 
-STATIC_ROOT = os.path.join(BASE_DIR,'doc/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'doc/static/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
